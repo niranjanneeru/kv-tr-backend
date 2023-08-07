7e4db767f57d081b8fabfac144b4152d2e017409
Niranjan B <niranjannb7777@gmail.com>
2023-08-07 14:32:44 +0530

    Changed Response Code

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -3,6 +3,7 @@ import RequestWithLogger from "../utils/request.logger"
 import { validate as v } from "class-validator";
 import ValidationException from "../exception/validation.exception";
 import { plainToInstance } from "class-transformer";
+import { StatusCodes } from "../utils/status.code.enum";
 
 const validateMiddleware = (cls, options = {}) => {
     return async (req: RequestWithLogger, res: Response, next: NextFunction) => {
@@ -10,7 +11,7 @@ const validateMiddleware = (cls, options = {}) => {
             const dto = plainToInstance(cls, req.body);
             const errors = await v(cls, options);
             if (errors.length > 0) {
-                throw new ValidationException(400, "Validation Errors", errors);
+                throw new ValidationException(StatusCodes.BAD_REQUEST, "Validation Errors", errors);
             }
             req.dto = dto;
             next();
